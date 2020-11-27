52bfe22dda839aaa02db5446515a2a17a2a77318
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-11-27 23:53:49 +0700

    Create P8.py

---
+++ P8.py
@@ -0,0 +1,28 @@
+#Project8
+
+fruit = { 'apel'  : 5000,
+          'jeruk' : 8500,
+          'mangga': 7800,
+          'duku'  : 6500 }
+
+def rata2HargaFruit(fruit):
+    totalHarga = 0
+    jumlah = 0
+
+    for r,y in buah.items():
+        totalHarga += y
+        jumlah += 1
+
+    rata2 = totalHarga/jumlah
+    return rata2
+
+def ratarataHargaFruit(fruit):
+    harga = list(fruit.values())
+    rata = sum(harga)/len(harga)
+    return rata
+
+a = ratarataHargaFruit(fruit)
+b = ratarataHargaFruit(fruit)
+
+print(a)
+print(b)
