9f14a46c0a7871479832ec3e22aad1cf8bd5c63e
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-11-27 23:53:44 +0700

    Create P6.py

---
+++ P6.py
@@ -0,0 +1,11 @@
+#Project6
+
+def sortStringByChar(data) :
+
+    data.sort(reverse = True)
+    return data
+
+data = ['apel','rambutan','jeruk']
+dataSorted = sortStringByChar(data)
+
+print('Susunan data yang baru :', dataSorted)
