c7ca2d6a9744cd7d9fe25aafd1461b2bc733d987
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-11-27 23:53:38 +0700

    Create P3.py

---
+++ P3.py
@@ -0,0 +1,26 @@
+#Project3
+
+dataNama = []
+r = True
+
+while(r == True) :
+    nama = input('Masukkan nama :')
+    dataNama.append(nama)
+
+    lanjut = input('Apakah ingin menginput nama lagi ? (y/n)')
+
+    if(lanjut == 'y') :
+        r = True
+
+    elif(lanjut == 'n') :
+        r = False
+
+    else :
+        print('Input tidak valid')
+        break
+
+print()
+dataNama.sort()
+
+for r in range(len(dataNama)) :
+    print(dataNama[r], '(', len(dataNama[r]), 'karakter )')
