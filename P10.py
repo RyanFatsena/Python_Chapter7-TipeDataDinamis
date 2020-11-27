fd3662580397ffb4275f1ea53da85a80b99ccdc3
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-11-27 23:53:53 +0700

    Create P10.py

---
+++ P10.py
@@ -0,0 +1,57 @@
+#Project10
+
+fruit = { 'apel'  : 5000,
+          'jeruk' : 8500,
+          'mangga': 7800,
+          'duku'  : 6500 }
+
+def checkOut(databaseFruit,namaFruit,banyakKilo):
+    hargaFruit = databaseFruit.get(namaFruit)
+    totalHarga = hargaFruit * banyakKilo
+    return totalHarga
+
+totalHarga = 0
+
+selesai = False
+
+while not(selesai):
+    while True:
+        namaFruit = str(input('Nama fruit yang akan dibeli                    : '))
+        
+        if(not(namaFruit in fruit.keys())):
+            print('Nama fruit tidak ditemukan')
+            continue
+        
+        else:
+            break
+        
+    while True:  
+        try:
+            perkilo = float(input('Berapa Kg (jika bilangan desimal gunakan titik): '))
+
+        except ValueError:
+            print('Mohon masukkan dengan angka')
+            continue
+        
+        else:
+            break
+        
+    totalHarga = totalHarga + checkOut(fruit,namaFruit,perkilo)
+    
+    choice = None
+    
+    while choice not in('y','n'):
+        choice = str(input('Beli lagi (y/n)? '))
+        
+        if(choice == 'y'):
+            selesai = False
+            print('')
+            
+        elif(choice == 'n'):
+            selesai = True
+            
+        else:
+            print('Mohon masukkan pilihan (y/n)')
+            
+print('--------------------------------------------------------')
+print('Total harga                                    : {0}'.format(totalHarga))
