233dcbdbe2bcf7c6e4532ce0f8c9c39afe47455c
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-11-27 23:53:47 +0700

    Create P7.py

---
+++ P7.py
@@ -0,0 +1,19 @@
+#Project7
+
+fruit = { 'apel'  : 5000,
+          'jeruk' : 8500,
+          'mangga': 7800,
+          'duku'  : 6500 }
+
+def expensive(dictionary) :
+
+    key = list(dictionary.keys())
+    values = tuple(dictionary.values())
+
+    hargaExpensive = max(values)
+
+    indexHargaExpensive = values.index(hargaExpensive)
+
+    print(key[indexHargaExpensive])
+
+expensive(fruit)
